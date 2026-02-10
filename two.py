fa559ee77da48633423de8f725c2687e28f4b1a0
1234-000 <wilsonm68@nycstudents.net>
2026-02-10 08:59:29 -0500

    wewewe

--- two.py
+++ two.py
@@ -1,5 +1,12 @@
-x = "test"
-print(f"hello {x}")
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
+
+for i in range(1, num1 + 1):
+    if i <= num2:
+        if num1%i==0 and num2%i==0:
+            gcf = i
+            
+print ("Factors are", gcf)
 
 
 
